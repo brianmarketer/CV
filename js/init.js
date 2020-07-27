3e68aef76a9ee3f8c438b1015a5f58526b4b79ad
brianmarketer <57597021+brianmarketer@users.noreply.github.com>
2020-07-28 01:07:27 +0700

    Content Changed 28/7/20

--- js/init.js
+++ js/init.js
@@ -174,7 +174,7 @@ var eric = function (){
 	
 	/*Typed js start*/
 	$("#typed").typed({
-		strings: ["^2100&nbsp;I'm Hoàng Bảo,<br/>An enthusiastic Marketer seeking for new challenge!"],
+		strings: ["^2100&nbsp;I'm Hoàng Bảo,<br/>A Marketer seeking for new challenge!"],
 		typeSpeed: 30,
 		backDelay: 750,
 		loop: false,
